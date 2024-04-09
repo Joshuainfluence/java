b037dc5211973851418480cfdac1ae56b395c356
unknown <joshuajulius2030@gmail.com>
2024-04-09 08:33:08 +0100

    initialized

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("I");
+        System.out.println("It is really good");
+        // this next of code is for quote
+        /*
+         * this 
+         * is 
+         * a 
+         * very 
+         * good 
+         * comment
+         * please read my code
+         */
+        System.out.println("\"i am what i am this is the word of the lord\"");
+
+        // working with variables
+        int x = 12; //initialization
+
+        System.out.print(x);
+    }
+}
